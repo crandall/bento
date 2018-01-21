7de9a801cca05bc72a477bd98d34e3bd57d9197d
crandall <michaelgcrandall@yahoo.com>
2018-01-21 12:47:36 -0700

    add controllers

---
+++ bento/GameplayController.swift
@@ -0,0 +1,35 @@
+//
+//  GameplayController.swift
+//  bento
+//
+//  Created by Mike Crandall on 1/21/18.
+//  Copyright © 2018 Eyebrowman. All rights reserved.
+//
+
+import UIKit
+
+class GameplayController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
